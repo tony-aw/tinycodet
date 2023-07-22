c414e9230f7d64d70fdc93d6e8518f1ddac4a060
Tony <66729956+tony-aw@users.noreply.github.com>
2023-07-22 10:17:25 +0200

    Add files via upload

---
+++ vignettes/web_only/sourcetest.R
@@ -0,0 +1,4 @@
+helloworld <- function() print("hello world")
+goodbyeworld <- function() print("goodbye world")
+`%s+test%` <- function(x,y) stringi::`%s+%`(x,y)
+`%s*test%` <- function(x,y) stringi::`%s*%`(x,y)
