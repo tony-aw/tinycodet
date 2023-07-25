99e893f6f59daa86cd925b760395d982b9f4f4dc
Tony <66729956+tony-aw@users.noreply.github.com>
2023-07-25 21:04:46 +0200

    Delete vignettes/web_only directory

--- vignettes/web_only/sourcetest.R
+++
@@ -1,4 +0,0 @@
-helloworld <- function() print("hello world")
-goodbyeworld <- function() print("goodbye world")
-`%s+test%` <- function(x,y) stringi::`%s+%`(x,y)
-`%s*test%` <- function(x,y) stringi::`%s*%`(x,y)
