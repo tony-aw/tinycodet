4ec3c9ab4ecd4da11d5d691385c439bd757c66e4
Tony <66729956+tony-aw@users.noreply.github.com>
2023-09-18 16:29:01 +0200

    LAST MAJOR UPDATE (I promise): changed the (yet again) to `tinycodet`. R package is now basically finished, safer for some teeny tiny textual edits in the documentation here and there.

---
+++ vignettes/articles/speedtest.R
@@ -0,0 +1,37 @@
+library(tinycodet)
+library(rbenchmark)
+
+n <- 1e5
+x <- rep("hello", n)
+i <- sample(1:3, n, replace = TRUE)
+loc <- stri_locate_ith(x, i=i, regex="a|e|i|o|u")
+benchmark.strcut <- benchmark(
+  "strcut_loc" = { strcut_loc(x, loc) },
+  "strcut_brk" = { strcut_brk(x, brk="chr") },
+  "stringi::stri_split_boundaries" = {
+    stringi::stri_split_boundaries(x, type="character")
+  },
+  replications = 500,
+  order = NULL,
+  columns = c("test", "replications", "elapsed",
+              "relative", "user.self", "sys.self")
+)
+
+n <- 1e5
+x <- rep(paste0(1:100, collapse=""), n)
+p <- "\\d"
+i <- sample(c(-50:-1, 1:50), replace=TRUE, size = n)
+locate_stringi <- function(...) {
+  stringi::stri_locate_all(...)
+  stringi::stri_count(...)
+}
+benchmark.stri_locate_ith <- benchmark(
+  "stri_locate_ith" = { stri_locate_ith(x, i, regex=p) },
+  "locate_stringi" = { locate_stringi(str=x, regex=p) },
+  replications = 500,
+  order = NULL,
+  columns = c("test", "replications", "elapsed",
+              "relative", "user.self", "sys.self")
+)
+
+save(benchmark.strcut, benchmark.stri_locate_ith, file = "speedtest.RData")
