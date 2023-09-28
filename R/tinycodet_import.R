c24e0df6c7cf685d52b375fd03e8b628b8124b16
Tony <66729956+tony-aw@users.noreply.github.com>
2023-09-28 19:34:41 +0200

    Minor textual corrections in documentation and website.

--- R/tinycodet_import.R
+++ R/tinycodet_import.R
@@ -3,37 +3,38 @@
 #' @description
 #' The tinycodet R package introduces a new package import system. \cr
 #' \cr
-#' One can load a package \bold{without} attaching the package -
-#' i.e. using \link[base]{::} ("colons") or aliasing the package. \cr
-#' Or, one can \bold{attach} a package by using \link[base]{library} or \link[base]{require}. \cr
+#' One can use a package \bold{without attaching} the package -
+#' for example by using the \link[base]{::} operator. \cr
+#' Or, one can explicitly \bold{attach} a package -
+#' for example by using the \link[base]{library} function. \cr
 #' The advantages and disadvantages
 #' of loading without attaching a package versus attaching a package,
 #' at least those relevant here,
 #' are compactly presented in the following list: \cr
 #' \cr
 #' (1) Prevent masking functions from other packages: \cr
-#' `r .mybadge_import("alias or colons", "Yes(advantage)", "darkgreen")` \verb{ }
+#' `r .mybadge_import("use without attach", "Yes(advantage)", "darkgreen")` \verb{ }
 #' `r .mybadge_import("attaching", "No(disadvantage)", "red")` \cr
 #' \cr
 #' (2) Prevent masking core R functions: \cr
-#' `r .mybadge_import("alias or colons", "Yes(advantage)", "darkgreen")` \verb{ }
+#' `r .mybadge_import("use without attach", "Yes(advantage)", "darkgreen")` \verb{ }
 #' `r .mybadge_import("attaching", "No(disadvantage)", "red")` \cr
 #' \cr
 #' (3) Clarify which function came from which package: \cr
-#' `r .mybadge_import("alias or colons", "Yes(advantage)", "darkgreen")` \verb{ }
+#' `r .mybadge_import("use without attach", "Yes(advantage)", "darkgreen")` \verb{ }
 #' `r .mybadge_import("attaching", "No(disadvantage)", "red")` \cr
 #' \cr
 #' (4) Place/expose functions only in current environment instead of globally: \cr
-#' `r .mybadge_import("alias or colons", "Yes(advantage)", "darkgreen")` \verb{ }
+#' `r .mybadge_import("use without attach", "Yes(advantage)", "darkgreen")` \verb{ }
 #' `r .mybadge_import("attaching", "No(disadvantage)", "red")` \cr
 #' \cr
 #' (5) Prevent namespace pollution: \cr
-#' `r .mybadge_import("alias or colons", "Yes(advantage)", "darkgreen")` \verb{ }
+#' `r .mybadge_import("use without attach", "Yes(advantage)", "darkgreen")` \verb{ }
 #' `r .mybadge_import("attaching", "No(disadvantage)", "red")` \cr
 #' \cr
 #' (6) Minimize typing - especially for infix operators \cr
 #' (i.e. typing ``package::`%op%`(x, y)`` instead of \code{x %op% y} is cumbersome): \cr
-#' `r .mybadge_import("alias or colons", "No(disadvantage)", "red")` \verb{ }
+#' `r .mybadge_import("use without attach", "No(disadvantage)", "red")` \verb{ }
 #' `r .mybadge_import("attaching", "Yes(advantage)", "darkgreen")` \cr
 #' \cr
 #' (7) Use multiple related packages,
@@ -41,7 +42,7 @@
 #' (i.e. doing \code{packagename1::some_function1()}; \cr
 #' \code{packagename2::some_function2()}; \cr
 #' \code{packagename3::some_function3()} is chaotic and cumbersome): \cr
-#' `r .mybadge_import("alias or colons", "No(disadvantage)", "red")` \verb{ }
+#' `r .mybadge_import("use without attach", "No(disadvantage)", "red")` \verb{ }
 #' `r .mybadge_import("attaching", "Yes(advantage)", "darkgreen")` \cr
 #' \cr
 #'
@@ -52,7 +53,7 @@
 #' - \link{import_as}:
 #' Allow a main package + its re-exports + its dependencies + its extensions to be loaded under a single alias.
 #' This essentially combines the attaching advantage of using multiple related packages (item 7 on the list),
-#' whilst keeping most advantages of aliasing a package.
+#' whilst keeping most advantages of using without attaching a package.
 #' - \link{import_inops}:
 #' Expose infix operators from a package or an alias object to the current environment.
 #' This gains the attaching advantage of less typing (item 6 on the list),
@@ -66,6 +67,7 @@
 #' And there are also some additional helper functions for the package import system,
 #' see \link[=help.import]{x.import} and \link[=pkg_get_deps]{pkgs}. \cr
 #' \cr
+#' See the examples below to get an idea how this works.
 #'
 #'
 #'
