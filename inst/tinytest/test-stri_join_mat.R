f05d4e87f39a6ec0bcfb5fbaa1d3e763ce011a52
Tony <66729956+tony-aw@users.noreply.github.com>
2023-09-30 12:56:45 +0200

    More examples added. Tiny textual edits to documentation.

--- inst/tinytest/test-stri_join_mat.R
+++ inst/tinytest/test-stri_join_mat.R
@@ -2,13 +2,13 @@
 
 expected <- c("abcde", "fghij", "klmno", "pqrst", "uvwxy")
 
-expect_equal(matrix(letters[1:25], ncol=5, byrow = T) |> stri_join_mat(margin=1),
+expect_equal(matrix(letters[1:25], ncol=5, byrow = TRUE) |> stri_join_mat(margin=1),
              expected)
-expect_equal(matrix(letters[1:25], ncol=5, byrow = F) |> stri_join_mat(margin=2),
+expect_equal(matrix(letters[1:25], ncol=5, byrow = FALSE) |> stri_join_mat(margin=2),
              expected)
 
 expect_error(
-  matrix(letters[1:25], ncol=5, byrow = T) |> stri_join_mat(margin=3),
+  matrix(letters[1:25], ncol=5, byrow = TRUE) |> stri_join_mat(margin=3),
   pattern = "`margin` must be either 1 or 2"
 )
 
