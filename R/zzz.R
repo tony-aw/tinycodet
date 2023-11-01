4e7e64bd0a208ec7aa25691b0a724fbfae3fcdbe
Tony <66729956+tony-aw@users.noreply.github.com>
2023-11-01 20:05:54 +0100

    Delete R directory

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  txt <- paste0(
-    "Run `",
-    '?tinycodet::tinycodet',
-    "` to open the introduction help page of tinycodet."
-  )
-  packageStartupMessage(txt)
-}
