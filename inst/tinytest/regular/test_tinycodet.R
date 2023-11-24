a10662d0693cbd1ab4fbc3c43313e4c0ea8ed951
Tony <66729956+tony-aw@users.noreply.github.com>
2023-11-24 19:46:52 +0100

    Update test_tinycodet.R

--- inst/tinytest/regular/test_tinycodet.R
+++ inst/tinytest/regular/test_tinycodet.R
@@ -3,6 +3,6 @@
 expect_equal(1 + 1, 2)
 expect_message(
   tinycodet:::.onAttach(),
-  pattern = "Run `?tinycodet::tinycodet` to open the introduction help page of tinycodet.",
+  pattern = "Run `?tinycodet::tinycodet` to open the introduction help page of 'tinycodet'.",
   fixed = TRUE
 )
