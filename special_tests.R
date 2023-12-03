7846cdb3ab8180158347ba0eae31f14ae4472694
Tony <tony_a_wilkes@outlook.com>
2023-12-03 12:43:25 +0100

    Create special_tests.R

---
+++ special_tests.R
@@ -0,0 +1,61 @@
+# special _import_as() tests
+library(tinycodet)
+library(tinytest)
+
+# import_as ====
+import_as(~ dpr., "dplyr", re_exports = TRUE)
+out <- setdiff(names(dpr.), ".__attributes__.") |> sort()
+foo <- loadNamespace("dplyr") |> getNamespaceExports()
+check <- all(out == sort(foo))
+expect_true(check)
+
+expect_error(
+  import_as(~ ms., "MASS", extensions = "ggplot2"),
+  "The following given extensions were not found to be actual extensions:",
+  fixed = TRUE
+)
+
+expect_error(
+  import_as(~ rl., "rlang", extensions = "ggplot2"),
+  "The following given extensions were not found to be actual extensions:",
+  fixed = TRUE
+)
+
+expect_error(
+  import_as(~ rsa., "rstudioapi", extensions = "tidyverse"),
+  "The following given extensions were not found to be actual extensions:",
+  fixed = TRUE
+)
+
+
+import_as(~ gg., "ggplot2", dependencies = "MASS")
+expect_error(
+  import_as(~ new., "ggplot2",
+            dependencies = pkg_get_deps("ggplot2")
+  ),
+  pattern = "more than 10 packages not allowed to be loaded under a single alias"
+)
+
+# is.tinyimport & help.import() ====
+import_inops("magrittr")
+import_as(~mr., "magrittr")
+`:=` <- data.table::`:=`
+expect_true(is.tinyimport(mr.))
+expect_true(is.tinyimport(`%>%`))
+expect_false(is.tinyimport(`:=`))
+alias_attr <- mr.$.__attributes__.
+str(alias_attr)
+attr.import(mr., "pkgs")
+attr.import(mr., "conflicts")
+attr.import(mr., "versions")
+attr.import(mr., "args")
+attr.import(mr., "ordered_object_names")
+expect_error(
+  attr.import(mr., "foo"),
+  pattern = "unknown which given"
+)
+help.import(i = `%>%`)
+help.import(i = mr.$multiply_by)
+help.import(i = "%>%", alias=mr.)
+help.import(i = "add", alias=mr.)
+
