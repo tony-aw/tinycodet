8fcb40b53752e81181da20f4a7a9ba79a91608cc
Tony <tony_a_wilkes@outlook.com>
2024-05-06 17:54:34 +0200

    Removed duplicate tests

--- inst/tinytest/special/test-import_int-special.R
+++ inst/tinytest/special/test-import_int-special.R
@@ -11,8 +11,6 @@ print(lib.loc2)
 print(lib.loc3)
 
 
-
-# general checks ====
 expect_equal(
   import_int(tinycodet ~ .internal_prep_Namespace),
   tinycodet:::.internal_prep_Namespace
@@ -34,53 +32,6 @@ expect_error(
 )
 
 
-# formula error handling ====
-expect_error(
-  import_int(""),
-  pattern = "`form` must be a formula"
-)
-
-expect_error(
-  import_int(~ .foo),
-  pattern = "`form` must be a two-sided formula"
-)
-
-expect_error(
-  import_int(stringi + gamair ~ foo),
-  pattern = "must give a single package"
-)
-
-expect_error(
-  import_int(tinycodet ~ .internal_prep_Namespace + .internal_check_conflicting_inops),
-  pattern = "must give a single internal function"
-)
-
-
-# package error handling ====
-expect_error(
-  import_int(stringi ~ stri_detect, lib.loc = "foo"),
-  pattern = "The following packages are not installed"
-)
-
-expect_error(
-  import_int(stringi ~ stri_detect, lib.loc = c("foo", "foo")),
-  pattern = "The following packages are not installed"
-)
-
-expect_error(
-  import_int(base ~ loadNamespace),
-  pattern = 'The following "packages" are base/core R, which is not allowed:'
-)
-
-expect_error(
-  import_int(`!foo` ~ foo),
-  pattern = "You have misspelled the following"
-)
-
-expect_error(
-  import_int(stringi ~ .foo, lib.loc = mean),
-  pattern = "`lib.loc` must be a character vector with at least one library path"
-)
 
 # clean-up ====
 # dir2remove <- file.path(to.dir, list.files(to.dir)) |> normalizePath()
